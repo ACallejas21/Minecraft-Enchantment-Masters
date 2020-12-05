f3bc354f0c8128a43e5e8e5aff26ea22b1b226d6
Akdiel Callejas <Akdiel.callejas21@gmail.com>
2020-12-04 20:06:31 -0600

    Inicio de diseño pantalla Home

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import {Button, Icon} from "native-base";
+import { AntDesign } from '@expo/vector-icons'; 
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Dimensions, ser} from 'react-native';
+import {Button, Icon, Picker, Image, Header, Item, Input, Right} from "native-base";
 
 
 const { width, height } = Dimensions.get("window");
 
 const HomeScreen = ({navigation }) => {
+  const [search, setSearch] = useState("");
     return( 
-    <View style={styles.container}>
-      <Text>Pantalla de Homesss!</Text>
-      <StatusBar style="auto" />
-      <Button onPress={() =>navigation.navigate("Tools")} style={styles.searchButton}>
-          <Icon name="search" />
-        </Button>
-    </View>
+      <View style={{backgroundColor: '#DBDBDB'}}>
+        <Header searchBar transparent androidStatusBarColor='#F92626' style={styles.headerStyle}>
+          <Item style={styles.itemlogo}>
+            <Input placeholder="Search" value={search} onChangeText={setSearch}/>
+          </Item>
+          <Right style={styles.searchButton}>
+              <Button icon transparent onPress={() => handlerSearch()}>
+                <Icon><AntDesign name="search1" size={24} color="red" /></Icon>
+              </Button>
+            </Right>
+      </Header>
+      <View>
+
+      </View>
+      </View>
     );
 };
 
@@ -30,6 +40,24 @@ const styles = StyleSheet.create({
     backgroundColor: "#7ae582",
     marginLeft: 10,
     height: 40,
+  },
+  
+  itemlogo:{
+    flex: 1,
+    borderRadius: 35,
+    paddingLeft: 10,
+  },
+  
+  searchButton:{
+    flex: 0.19,
+    borderRadius: 15,
+    color: 'red',
+  },
+  headerStyle:{
+    backgroundColor: '#DBDBDB',
+    height: height / 15,
+    borderWidth: 0.23,
+    borderColor: "black"
   },
 });
 
